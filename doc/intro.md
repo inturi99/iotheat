2d07a1069f6630c90e5ca5f6b5d8417d18f8268f
abelia <inturi99@gmail.com>
2015-12-07 16:09:00 +0530

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to iotheat
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/iotheat/core_test.clj
@@ -0,0 +1,7 @@
+(ns iotheat.core-test
+  (:require [clojure.test :refer :all]
+            [iotheat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
