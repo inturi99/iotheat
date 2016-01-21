1d18da350f5017eeac2cfa8fc089f268f36dbf96
abelia <inturi99@gmail.com>
2016-01-21 13:12:20 +0530

    update get-uid-all-deviceheat

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -2,7 +2,7 @@
 SELECT * FROM deviceheat WHERE uid = :uid ORDER BY createdatetime DESC LIMIT 1
 
 --name: get-uid-all-deviceheat
-SELECT * FROM deviceheat WHERE uid= :uid ORDER BY createdatetime DESC
+SELECT * FROM deviceheat WHERE lower(uid) LIKE ('%' || lower(:uid) || '%') ORDER BY createdatetime DESC
 
 --name: get-all-deviceheat-latest
 SELECT DISTINCT ON(uid) * FROM deviceheat ORDER BY uid,createdatetime DESC
